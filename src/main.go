b7631445f5a211920f02830fb67e92e90e0aaf14
Sam Westmoreland <samwestmoreland@gmail.com>
2025-05-11 19:46:58 +0100

    feat: handle versioning

--- src/main.go
+++ src/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
+var Version string
+
 func main() {
+	log.Printf("Starting server version %s", Version)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello from Raspberry Pi!")
 	})
